e8f8e7186edab500b113d462db3f01a58622d9b8
Woody <kazettique@gmail.com>
2022-10-13 22:28:00 +0800

    add computed

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -1,18 +1,25 @@
 <script setup lang="ts">
-import { reactive, ref } from "vue";
+import { computed, reactive, ref } from "vue";
 
 const state = reactive({ count: 0 });
 const countRef = ref(5566);
 
-// console.log("countRef :>> ", countRef);
-// console.log("countRef.value :>> ", countRef.value);
-
+// update state
 function increment() {
   state.count++;
 }
 
+// update ref
 function updateRef() {
   countRef.value = countRef.value + 1;
+}
+
+// computed - 1: with computed caching
+const isOdd = computed(() => state.count % 2 === 1);
+
+// computed - 2: without computed caching
+function isOdd2() {
+  return state.count % 2 === 1;
 }
 </script>
 
@@ -22,6 +29,10 @@ function updateRef() {
     <button class="btn" @click="increment">
       {{ state.count }}
     </button>
+    <div>isOdd: {{ isOdd }}</div>
+    <div>isOdd2: {{ isOdd2() }}</div>
+  </div>
+  <div>
     <button class="btn" @click="updateRef">update ref</button>
     <div>{{ countRef }}</div>
   </div>
